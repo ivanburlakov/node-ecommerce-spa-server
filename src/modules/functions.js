00c5b1f192bf11230259ac0f20a4b47ef82d336c
Ivan Burlakov <burlakovv.ivan@gmail.com>
2020-12-23 15:08:54 +0200

    comment non working code

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -21,60 +21,61 @@ function jsonResult(result) {
 }
 
 // TODO: unsequelize
-async function getUserID(userData) {
-  try {
-    const { email, phone } = userData;
-    const where = { email, phone };
-    const attributes = ['user_ID'];
-    let user = await Users.findOne({ attributes, where });
-    if (user) return user.user_ID;
-    user = await Users.create(where);
-    return user.user_ID;
-  } catch (err) {
-    console.error('Unable to get user id: ', err);
-  }
-}
 
-async function addOrders(user_ID, orders) {
-  try {
-    let { product_ID, quantity, delivery_address } = orders.pop();
-    const firstQuery = { user_ID, product_ID, quantity, delivery_address };
-    const firstOrder = await Orders.create(firstQuery);
-    const { order_ID } = firstOrder;
-    const orderArray = [];
-    orders.forEach(element => {
-      ({ product_ID, quantity, delivery_address } = element);
-      const order = {
-        order_ID,
-        user_ID,
-        product_ID,
-        quantity,
-        delivery_address,
-      };
-      orderArray.push(order);
-    });
-    await Orders.bulkCreate(orderArray);
-  } catch (err) {
-    console.error('Unable to add order: ', err);
-  }
-}
+// async function getUserID(userData) {
+//   try {
+//     const { email, phone } = userData;
+//     const where = { email, phone };
+//     const attributes = ['user_ID'];
+//     let user = await Users.findOne({ attributes, where });
+//     if (user) return user.user_ID;
+//     user = await Users.create(where);
+//     return user.user_ID;
+//   } catch (err) {
+//     console.error('Unable to get user id: ', err);
+//   }
+// }
 
-async function postOrder(req, res) {
-  const { user, order } = req.body.message;
-  if (!req.body) return res.sendStatus(400);
-  user.phone = decodeURIComponent(user.phone);
-  user.email = decodeURIComponent(user.email);
-  const modifiedOrder = [];
-  order.forEach(el => {
-    const element = el;
-    element.delivery = decodeURIComponent(el.delivery);
-    modifiedOrder.push(element);
-  });
-  const userID = await getUserID(user);
-  const sendOrder = await addOrders(userID, modifiedOrder);
-  if (!userID || !sendOrder) return 0;
-  return 1;
-}
+// async function addOrders(user_ID, orders) {
+//   try {
+//     let { product_ID, quantity, delivery_address } = orders.pop();
+//     const firstQuery = { user_ID, product_ID, quantity, delivery_address };
+//     const firstOrder = await Orders.create(firstQuery);
+//     const { order_ID } = firstOrder;
+//     const orderArray = [];
+//     orders.forEach(element => {
+//       ({ product_ID, quantity, delivery_address } = element);
+//       const order = {
+//         order_ID,
+//         user_ID,
+//         product_ID,
+//         quantity,
+//         delivery_address,
+//       };
+//       orderArray.push(order);
+//     });
+//     await Orders.bulkCreate(orderArray);
+//   } catch (err) {
+//     console.error('Unable to add order: ', err);
+//   }
+// }
+
+// async function postOrder(req, res) {
+//   const { user, order } = req.body.message;
+//   if (!req.body) return res.sendStatus(400);
+//   user.phone = decodeURIComponent(user.phone);
+//   user.email = decodeURIComponent(user.email);
+//   const modifiedOrder = [];
+//   order.forEach(el => {
+//     const element = el;
+//     element.delivery = decodeURIComponent(el.delivery);
+//     modifiedOrder.push(element);
+//   });
+//   const userID = await getUserID(user);
+//   const sendOrder = await addOrders(userID, modifiedOrder);
+//   if (!userID || !sendOrder) return 0;
+//   return 1;
+// }
 
 async function updateJson() {
   try {
