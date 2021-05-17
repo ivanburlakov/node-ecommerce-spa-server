52f57bda29ae1f2d0e43c81198aec236c214577c
Ivan Burlakov <burlakovv.ivan@gmail.com>
2021-05-17 10:51:19 +0300

    update CI

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "testEnvironment": "node",
+  "modulePathIgnorePatterns": [
+    ".github",
+    "node_modules",
+    ".history",
+    "build",
+    "public"
+  ],
+  "setupFiles": ["<rootDir>/tests/setup-tests.js"],
+};
+
